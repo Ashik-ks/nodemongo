b81e3085c4dd618186a07a29555b451eea5fd339
ashik <ashik123ks@gmail.com>
2024-09-01 22:03:33 +0530

    first commit

---
+++ client/script.js
@@ -0,0 +1,87 @@
+async function addUser(event){
+    event.preventDefault();
+    console.log("Reached here")
+
+    let id = document.getElementById('id').value;
+    let title = document.getElementById('title').value;
+    let description = document.getElementById('description').value;
+    let category = document.getElementById('category').value;
+    let price = document.getElementById('price').value;
+    let imageurl = document.getElementById('imageurl').value;
+    let ratingrate = document.getElementById('ratingrate').value;
+    let ratingcount = document.getElementById('ratingcount').value;
+
+    let iderror = document.getElementById('id-error')
+    let titleerror = document.getElementById('title-error')
+    let descriptionerror = document.getElementById('description-error')
+    let categoryerror = document.getElementById('category-error')
+    let priceerror = document.getElementById('price-error')
+    let imageurlerror = document.getElementById('imageurl-error')
+    let ratingrateerror = document.getElementById('ratingrate-error')
+    let ratingcounterror = document.getElementById('ratingcount-error')
+
+
+
+    // let emailregex = /^[a-zA-Z0-9_-]+@[a-zA-Z]+\.[a-zA-Z]{2,}$/;
+    if(!id){
+        iderror.innerHTML = "id required"
+    }
+    if(!title){
+        titleerror.innerHTML = "title required"
+    }
+    if(!description){
+        descriptionerror.innerHTML = "description required"
+    }
+    if(!category){
+        categoryerror.innerHTML = "category required"
+    }
+    if(!price){
+        priceerror.innerHTML = "price required"
+    }
+    if(!imageurl){
+        imageurlerror.innerHTML = "imageurl required"
+    }
+    if(!ratingrate){
+        ratingrateerror.innerHTML = "ratingrate required"
+    }
+    if(!ratingcount){
+        ratingcounterror.innerHTML = "ratingcount required"
+    }
+
+    let datas = {
+        id,
+        title,
+        description,
+        category,
+        price,
+        imageurl,
+        ratingrate,
+        ratingcount,
+    }
+    
+    let json_data = JSON.stringify(datas)
+    console.log("json_data : ",json_data);
+
+    let response = await fetch('/submit',{
+        method : 'post',
+        headers : {
+            'Content-Type'  : "application/json"
+        },
+        body : json_data,
+    });
+    console.log("response : ",response); 
+
+    let parsed_response = await response.text();
+    console.log("parsed_response : ", parsed_response)
+
+    if(parsed_response) {
+        alert(parsed_response);
+        return;
+    }else {
+        alert("something went wrong");
+    }
+
+    
+
+
+}
---
+++ server/server.js
@@ -0,0 +1,167 @@
+const http = require('http');
+const PORT = 3000;
+const url = require('url');
+const fs = require('fs');
+const querystring = require('querystring');
+const {MongoClient} = require('mongodb');
+
+const server = http.createServer( async (req,res) => {
+
+    let client = new MongoClient('mongodb://localhost:27017');
+
+    async function connect(){
+        try {
+            await client.connect();
+            console.log("Database connection established")
+        } catch (error) {
+            console.log("error : ",error)
+        }
+    }
+
+    connect();
+
+    const req_url = req.url;
+    console.log("req_url : ",req_url);
+
+    const parsed_url = url.parse(req_url);
+    console.log("parsed_url : ",parsed_url);
+
+    if(parsed_url.pathname === '/'){
+        //serve the html file on root request
+    
+        res.writeHead(200,{'Content-Type' : 'text/html'});
+        res.end(fs.readFileSync('../client/index.html'));
+    }
+    if(parsed_url.pathname === '/adduser.html'){
+        //serve the html file on root request
+    
+        res.writeHead(200,{'Content-Type' : 'text/html'});
+        res.end(fs.readFileSync('../client/adduser.html'));
+    }
+    else if(parsed_url.pathname === '/style.css'){
+        res.writeHead(200,{'Content-Type' : 'text/css'});
+        res.end(fs.readFileSync('../client/style.css'));
+    }
+    else if(parsed_url.pathname === '/script.js'){
+        res.writeHead(200,{'Content-Type' : 'text/javascript'});
+        res.end(fs.readFileSync('../client/script.js'));
+    }
+    else if(parsed_url.pathname === '/submit'  &&  req.method === 'POST'){
+        console.log("reached here");
+
+        let body = '';
+
+        req.on('data',(chunks)=> {
+            console.log("chunks : ",chunks);
+            body += chunks.toString();
+        });
+
+        req.on('end',()=>{
+            let db = client.db("dms1");
+            let collection = db.collection("users1")
+            console.log("body : ",body)
+       
+        
+        let datas = JSON.parse(body);
+        console.log("datas : ",datas);
+         let id = datas.id;
+         let title = datas.title;
+         let description = datas.description;
+         let category = datas.category
+         let price = datas.price;
+         let imageurl = datas.imageurl;
+         let ratingrate = datas.ratingrate;
+         let ratingcount = datas.ratingcount;
+
+        console.log("datas :",datas.id)
+        console.log("datas :",datas.title)
+        console.log("datas :",datas.description)
+        console.log("category : ",category)
+        console.log("datas :",datas.price)
+        console.log("datas :",datas.imageurl)
+        console.log("datas :",datas.ratingrate)
+        console.log("datas :",datas.ratingcount)
+
+        if(!id){
+            res.writeHead(400,{'Content-Type' : 'text/plain'});
+            res.end("invalid id");
+            return;
+        }
+        if(!title){
+            res.writeHead(400,{'Content-Type' : 'text/plain'});
+            res.end("invalid title");
+            return;
+        }
+        if(!description){
+            res.writeHead(400,{'Content-Type' : 'text/plain'});
+            res.end("invalid description");
+            return;
+        }
+        if(!category){
+            res.writeHead(400,{'Content-Type' : 'text/plain'});
+            res.end("invalid category");
+            return;
+        }
+        if(!price){
+            res.writeHead(400,{'Content-Type' : 'text/plain'});
+            res.end("invalid price");
+            return;
+        }
+        if(!imageurl){
+            res.writeHead(400,{'Content-Type' : 'text/plain'});
+            res.end("invalid imageurl");
+            return;
+        }
+        if(!ratingrate){
+            res.writeHead(400,{'Content-Type' : 'text/plain'});
+            res.end("invalid ratingrate");
+            return;
+        }
+        if(!ratingcount){
+            res.writeHead(400,{'Content-Type' : 'text/plain'});
+            res.end("invalid ratingcount");
+            return;
+        }
+        collection.insertOne({
+            id : datas.id,
+            title : datas.title,
+            description: datas.description,
+            category : datas.category,
+            price: datas.price,
+            imageurl: datas.imageurl,
+            ratingrate: datas.ratingrate,
+            ratingcount: datas.ratingcount,
+            
+        })
+        .then((message) => {
+            console.log("message :",message);
+            res.writeHead(201,{'COntent-Type' : "text/plain"});
+            res.end("User created Succesfully");
+        })
+        .catch((error) => {
+            console.log("error : ",error);
+
+            res.writeHead(400,{'Content-Type' : "text/plain"});
+            res.end(error.message  ? error.message : "User creation failed");
+        })
+
+    });
+
+    }
+    else if (parsed_url.pathname === '/submit' && req.method === 'GET') {
+       
+        let userDatas = await  collection.find().toArray();
+        console.log("userDatas : ",userDatas);
+
+        let json_datas = JSON.stringify(userDatas);
+        console.log("json_datas : ",json_datas);
+
+        res.writeHead(200,{'Content-Type' : "adduser.html"});
+        res.end(fs.readFileSync('../client/adduser.html'));
+        // res.end(json_datas);
+    }
+
+})
+server.listen(PORT, () =>{
+    console.log(`server running at http://localhost:${PORT}`)
+});
